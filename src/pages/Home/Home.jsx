2a000025af0c333e8d4e8e4cad99ff248475d94f
Husain-Zreik <husain.zreik1@gmail.com>
2024-12-24 15:20:40 +0200

    edit the hero section view

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,14 @@ import Globe from '../../components/Globe/Globe';
 const Home = () => {
     return (
         <div className="page-home">
-            <div className="page-home__slice">
+            <div className="page-home__slice"></div>
+            <div className="page-home__content">
+                <h1 className="page-home__content--title">ProCom </h1>
+                <p className="page-home__content--subtitle">Crafting Code, Elevating Elegance.</p>
 
-                <div className="page-home__content">
-                    <h1 className="page-home__content--title">ProCom </h1>
-                    <p className="page-home__content--subtitle">Crafting Code, Elevating Elegance.</p>
+                <div className="page-home__content--buttons">
+                    <button className="page-home__content--button">Learn More</button>
+                    <button className="page-home__content--button">Get Started</button>
                 </div>
             </div>
 
