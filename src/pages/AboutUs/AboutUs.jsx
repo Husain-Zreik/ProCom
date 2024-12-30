f5cfb89982ed100c001096ff02a8200036d94ed0
Husain-Zreik <husain.zreik1@gmail.com>
2024-12-30 12:56:22 +0200

    edit about us namings

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -3,23 +3,20 @@ import SectionHeader from '../../components/SectionHeader/SectionHeader';
 
 function AboutUs() {
     return (
-        <section className="about-us">
-
-            <div className="about-us-container">
+        <section id='about' className="about">
+            <div className="about__container">
                 <SectionHeader title="About ProCom" />
-                <div className="about-us-content">
-                    {/* Left: Lottie animation */}
-                    <div className="about-us-animation">
+                <div className="about__content">
+                    <div className="about__media">
                         {/* <DotLottieReact
                             src="Welcome.json"
                             loop
                             autoplay
                         /> */}
-                        <img src="https://cdn.prod.website-files.com/63e3828f575098222357ad70/641b4a7b49dd292cb0ef5c7e_Untitled%20design%20(9)-p-500.png" alt={"hi"} className="about-us-image" />
+                        <img src="https://cdn.prod.website-files.com/63e3828f575098222357ad70/641b4a7b49dd292cb0ef5c7e_Untitled%20design%20(9)-p-500.png" alt={"hi"} className="about__image" />
                     </div>
 
-                    {/* Right: Company description */}
-                    <div className="about-us-description">
+                    <div className="about__text">
                         <p>
                             ProCom is a leading software company based in Qatar, focused on empowering businesses with innovative technology solutions. We specialize in custom software development, cloud services, and technical consultation. Our products, including eSIM activation, Bulk SMS marketing, and SaaS, are designed to deliver scalable, reliable, and cost-effective solutions tailored to our clients’ needs. For more details on our services, please explore our website.
                         </p>
