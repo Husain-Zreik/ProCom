067c4cde87512dc7fb94fba8f362038316a2975a
Husain-Zreik <husain.zreik1@gmail.com>
2024-12-30 08:57:42 +0200

    create section header structure and design

---
+++ src/components/SectionHeader/SectionHeader.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+const SectionHeader = ({ title, subtitle }) => {
+    return (
+        <div className="section-header">
+            <div className="section-header-right">
+                <i style={{ color: '#c54a5c' }} className="fa-solid fa-circle-notch fa-spin fa-2xl"></i>
+                <div className='section-header-content'>
+                    <h1 className="section-title">{title}</h1>
+                    {subtitle && <p className="section-subtitle">{subtitle}</p>}
+                </div>
+            </div>
+            <div className="section-icons">
+                <div style={{ backgroundColor: '#c54a5c' }} className="icon-shape">
+                    <i className="fa-solid fa-signature fa-fade"></i>
+                </div>
+                <div className="icon-shape">
+                    <i className="fa-solid fa-wave-square fa-flip"></i>
+                </div>
+                <div style={{ backgroundColor: '#131313' }} className="icon-shape">
+                    <i className="fa-brands fa-think-peaks fa-fade"></i>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+SectionHeader.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string, // Optional subtitle prop
+};
+
+export default SectionHeader;
